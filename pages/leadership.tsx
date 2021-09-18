2268d643deb3a1ee67b736861300a23f4aa606de
Joshua Schoen <joshuaschoen@Joshuas-MacBook-Pro.local>
2021-09-18 12:27:56 -0500

    added eslint and prettier

---
+++ pages/leadership.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Leadership: NextPage = () => {
+    return (
+        <div>leadership</div>
+    );
+ };
+
+ export default Leadership;
