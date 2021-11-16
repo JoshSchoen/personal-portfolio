311d069f9bffcd12f7156350bc855f3acecd85b1
Joshua Schoen <joshuaschoen@Joshuas-MacBook-Pro.local>
2021-11-16 14:54:55 -0600

    added skills component

--- components/Skils.tsx
+++ components/Skils.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Flex, useColorMode, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { Button, Flex, useColorMode, IconButton, useBreakpointValue, Heading } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
 export const Skills = () => {
@@ -8,13 +8,24 @@ export const Skills = () => {
     md: 'md'
   });
   const mode = colorMode === 'light' ? '' : '-dark';
+  const direction = variant !== 'md' ? 'column' : 'row';
 
   return (
       <>
-      
-    <Flex justifyContent="space-around" flexDirection={variant !== 'md' ? 'column' : 'row'}>
+            <Heading
+        marginTop="4"
+        as="h2"
+        fontSize="4xl"
+        textAlign="center" style={{padding: '64px 0px 32px 0px'}}>
+        Skills
+        </Heading>
+    <Flex width="100%" justifyContent="space-around" flexDirection={direction} alignItems="center">
+    <div style={{padding: '12px'}}>
       <img src={`/skills-radar${mode}.svg`} alt="Josh Schoen's design skills" />
+      </div>
+      <div style={{padding: '12px'}}>
       <img src={`/bar-chart${mode}.svg`} alt="Josh Schoen's technical skills" />
+      </div>
     </Flex>
     </>
   );
